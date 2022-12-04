280ee6907711cd061c3a55fdd7427ebb66f2ea46
araldwin <araldwin@gmail.com>
2022-12-04 13:07:11 +0000

    Add lightOn function

--- scripts/game.js
+++ scripts/game.js
@@ -22,4 +22,11 @@ function addTurn() {
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
-module.exports = { game, newGame, showScore, addTurn };
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
