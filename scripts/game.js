9901de2865d57fcd3432f749323d0b09d6ac2e13
araldwin <araldwin@gmail.com>
2022-12-04 13:16:52 +0000

    add showTurns function

--- scripts/game.js
+++ scripts/game.js
@@ -16,7 +16,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
 }
 
 function showScore() {
@@ -29,4 +29,15 @@ function lightsOn(circ) {
         document.getElementById(circ).classList.remove("light");
     }, 400);
 }
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
